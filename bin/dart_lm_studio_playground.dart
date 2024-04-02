428d58b2f538bbdc8bf947b66c2251c1b7a5f225
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2024-04-02 15:38:46 +0100

    first commit

---
+++ bin/dart_lm_studio_playground.dart
@@ -0,0 +1,5 @@
+import 'package:dart_lm_studio_playground/dart_lm_studio_playground.dart' as dart_lm_studio_playground;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_lm_studio_playground.calculate()}!');
+}
