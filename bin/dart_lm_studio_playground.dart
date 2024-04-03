ba9d3e2ff5022a86f848716eff79465c38715275
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2024-04-03 15:17:37 +0100

    added sys message to only return function_calling

--- bin/dart_lm_studio_playground.dart
+++ bin/dart_lm_studio_playground.dart
@@ -19,50 +19,120 @@ void main(List<String> arguments) async {
   //   "stream": false
   // });
   // print(response.data.toString());
-  Response<ResponseBody> rs = await dio.post<ResponseBody>('http://192.168.1.132:1234/v1/chat/completions',
-      options: Options(responseType: ResponseType.stream),
-      data: {
-        "model": "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q6_K.gguf",
-        "seed": DateTime.now().microsecondsSinceEpoch,
-        "messages": [
-          {"role": "system", "content": "You are an Fitness coach"},
-          {
-            "role": "user",
-            "content":
-                "Give me only one single line, original, short, fitness related, motivational quote. Do not include an explanation. Do not include who said it.  Do not include a hashtag."
-          }
-        ],
-        "temperature": 0.7,
-        "max_tokens": -1,
-        "stream": true
-      });
 
-  StreamTransformer<Uint8List, List<int>> unit8Transformer = StreamTransformer.fromHandlers(
-    handleData: (data, sink) {
-      sink.add(List<int>.from(data));
-    },
-  );
+//--------------------------------------------------
 
-  var contentSoFar = '';
+  // Response<ResponseBody> rs = await dio.post<ResponseBody>('http://192.168.1.132:1234/v1/chat/completions',
+  //     options: Options(responseType: ResponseType.stream),
+  //     data: {
+  //       //"model": "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q6_K.gguf",
+  //       "seed": DateTime.now().microsecondsSinceEpoch,
 
-  rs.data?.stream
-      .transform(unit8Transformer)
-      .transform(const Utf8Decoder())
-      .transform(const LineSplitter())
-      .listen((event) {
-    print(event);
-    if (event.length > 6) {
-      //print("*${event.substring(6, 7)}*");
-      if (event.substring(6, 7) == '{') {
-        Map<String, dynamic> j = json.decode(event.substring(6));
-        //print(j);
-        //print(j['choices'][0]['delta']['content']);
+  //       "messages": [
+  //         {"role": "system", "content": "You are an Fitness coach"},
+  //         {
+  //           "role": "user",
+  //           "content":
+  //               "Give me only one single line, original, short, fitness related, motivational quote. Do not include an explanation. Do not include who said it.  Do not include a hashtag."
+  //         }
+  //       ],
+  //       "temperature": 0.7,
+  //       "max_tokens": -1,
+  //       "stream": true
+  //     });
 
-        if (j['choices'][0]['delta']['content'] != null) {
-          contentSoFar = contentSoFar + j['choices'][0]['delta']['content'];
-        }
-        print(contentSoFar);
-      }
+  // StreamTransformer<Uint8List, List<int>> unit8Transformer = StreamTransformer.fromHandlers(
+  //   handleData: (data, sink) {
+  //     sink.add(List<int>.from(data));
+  //   },
+  // );
+
+  // var contentSoFar = '';
+
+  // rs.data?.stream
+  //     .transform(unit8Transformer)
+  //     .transform(const Utf8Decoder())
+  //     .transform(const LineSplitter())
+  //     .listen((event) {
+  //   print(event);
+  //   if (event.length > 6) {
+  //     //print("*${event.substring(6, 7)}*");
+  //     if (event.substring(6, 7) == '{') {
+  //       Map<String, dynamic> j = json.decode(event.substring(6));
+  //       //print(j);
+  //       //print(j['choices'][0]['delta']['content']);
+
+  //       if (j['choices'][0]['delta']['content'] != null) {
+  //         contentSoFar = contentSoFar + j['choices'][0]['delta']['content'];
+  //       }
+  //       print(contentSoFar);
+  //     }
+  //   }
+  // });
+
+//--------------------------------------------------
+
+  var sysInst = '''
+You are a helpful assistant with access to the following functions. Use them if required and only respond with JSON. - 
+{
+    "name": "order_pizza",
+    "description": "Order a pizza with custom toppings",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "size": {
+                "type": "string",
+                "description": "Size of the pizza (small, medium, large)"
+            },
+            "toppings": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "description": "List of toppings for the pizza"
+            }
+        },
+        "required": [
+            "size",
+            "toppings"
+        ]
     }
+}
+
+
+{
+    "name": "order_taxi",
+    "description": "Order a taxi to a destination",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "destination": {
+                "type": "string",
+                "description": "Destination of the taxi"
+            }
+        },
+        "required": [
+            "destination"
+        ]
+    }
+}
+
+If you cant match the request to a function, respond with {"error": "No function found"}
+
+''';
+
+  var response = await dio.post('http://192.168.1.132:1234/v1/chat/completions', data: {
+    "model": "TheBloke/Mistral-7B-Instruct-v0.2-GGUF/mistral-7b-instruct-v0.2.Q6_K.gguf",
+    "messages": [
+      {"role": "system", "content": sysInst},
+      //{"role": "user", "content": "I'd like a large pizza with anchovies and pepperoni"}
+      //{"role": "user", "content": "I'd like to go to Reading town centre."}
+      {"role": "user", "content": "Give me a recipe for a taco"}
+      //{"role": "user", "content": "Turn the lights on in the kitchen"}
+    ],
+    "temperature": 0.7,
+    "max_tokens": -1,
+    "stream": false
   });
+  print(response.data.toString());
 }
